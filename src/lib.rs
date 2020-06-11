ec0e4d38d6d103182cf27d78f4c377d4e97e4a9f
Charlie Zhu <goodgoodwish@gmail.com>
2020-06-11 10:53:32 -0700

    Implementing the Drop Trait on ThreadPool

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ type Job = Box<dyn FnOnce() + Send + 'static>;
 
 pub struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -20,7 +20,7 @@ impl Worker {
         });
         Worker {
             id,
-            thread,
+            thread: Some(thread),
         }
     }
 }
@@ -53,5 +53,16 @@ impl ThreadPool {
         let job = Box::new(f);
         self.sender.send(job).unwrap();
 
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            println!("");
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
